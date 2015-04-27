7ecb8cf23ab898e92866d8611da61c5fc406f4cc
Carsten Heinrigs <carsten@cahein.de>
2015-04-27 14:40:07 +0200

    spree 3.0.0 and cleanup

--- lib/generators/spree_menu_suckerfish/install/install_generator.rb
+++ lib/generators/spree_menu_suckerfish/install/install_generator.rb
@@ -6,25 +6,10 @@ module SpreeMenuSuckerfish
 
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_menu_suckerfish\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_menu_suckerfish\n"
       end
 
       def add_stylesheets
         inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_menu_suckerfish\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_menu_suckerfish\n", :before => /\*\//, :verbose => true
-      end
-
-      def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_menu_suckerfish'
-      end
-
-      def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
       end
     end
   end
